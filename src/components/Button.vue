1f88ecb1bf471c9a76073b2bd5d2fbc8fa7048fd
ns-morozova <128983577+ns-morozova@users.noreply.github.com>
2025-07-05 14:46:41 +0300

    Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,6 +1,8 @@
 <template>
     <button class="button" @click="$emit('click')">
-      {{ label }}
+      <div class="buttonContent">
+        {{ label }}
+      </div>
     </button>
 </template>
   
@@ -16,20 +18,59 @@ defineEmits<{
 
 <style scoped>
 .button {
+  position: relative;
   box-sizing: border-box;
-  padding: 0.75rem 1.5rem;
-  background-color: #000;
-  color: white;
-  border: 1px #000 solid;
+  padding: 0;
   outline: none;
-  border-radius: 0.375rem;
-  font-weight: 700;
+  border: none;
   cursor: pointer;
-  transition: all 0.3s ease;
+  z-index: 2;
 }
 
-.button:hover {
-  background-color: #fff;
-  color: #000;
+.buttonContent {
+  position: relative;
+  box-sizing: border-box;
+  padding: 16px;
+  border-radius: 8px;
+  background-color: #000;
+  color: white;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 700;
+  z-index: 2;
+}
+
+.button::before {
+  content: '';
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 110%;
+  height: 90%;
+  background-image: radial-gradient(
+    circle,
+    #44FF9A,
+    #44B0FF,
+    #8B44FF,
+    #FF6644,
+    #EBFF70
+  );
+  filter: blur(10px);
+  opacity: 0;
+  z-index: 1;
+  transition: opacity 0.3s ease;
+}
+
+.button:hover::before {
+  opacity: 1;
+}
+
+@media (min-width: 992px) {
+    .buttonContent {
+      padding: 12px 20px;
+      font-size: 14px;
+      line-height: 20px;
+    }
 }
 </style>
